419e29968efb11a10532262c4a33d7bd69b2a685
QuachDangDiep <145833428+QuachDangDiep@users.noreply.github.com>
2024-07-02 22:26:22 +0700

    .

--- my-react-app/src/Pages/Author/Author.jsx
+++ my-react-app/src/Pages/Author/Author.jsx
@@ -1,8 +1,213 @@
-import React from 'react'
+import React from 'react';
+import { RxSlash } from "react-icons/rx";
+import './Author.css';
 
 const Author = () => {
   return (
-    <div>Author</div>
+    <div className='author-main'>
+      <div className='author-page'>
+        <div className='author-title'>
+          <h1 className='author-product'> Product Author </h1>
+          <div className='author-bread'>
+            <div className='author-crumb'>
+              <a href='#' className='author-a'>Home</a>
+              <span className='author-delimiter'>
+                <i className='author-i'><RxSlash /></i>
+              </span>
+              <span className='author-current'>Product Author</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='author-container'>
+        <div className='author-row'>
+          <div className='author-col'>
+            <div className='author-content'>
+              <div className='author-area'>
+                <div className='author-site'>
+                  <article className='author-post'>
+                    <div className='author-entry'>
+                      <div className='author-custom'>
+                        <div className='author-column'>
+                          <div className='author-inner'>
+                            <div className='author-wrapper'>
+                              <div className='author-404'>
+                                <div className='authors'>
+                                  <div className='author-time'>
+                                    <div className='author-time-content'>
+                                      <div className='author-item-image'>
+                                        <a className='author-href'href='#'>
+                                          <img decoding='async' className='author-img' src='https://wpbingosite.com/wordpress/bootin/wp-content/uploads/2019/05/Image-1-1-1.jpg' alt='Anna Hillton'></img>
+                                        </a>
+                                      </div>
+                                      <div className='author-con'>
+                                        <div className='author-item-title'>
+                                          <a className='author-name' href=''>
+                                            <span className='author-span'>Anna Hillton</span>
+                                          </a>
+                                        </div>
+                                        <div className='author-item-count'>
+                                          3
+                                          <span className='author-book'> Published Book</span>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <div className='author-time'>
+                                    <div className='author-time-content'>
+                                      <div className='author-item-image'>
+                                        <a className='author-href'href='#'>
+                                          <img decoding='async' className='author-img' src='https://wpbingosite.com/wordpress/bootin/wp-content/uploads/2019/05/Image-2-2.jpg' alt='Anna Hilltons'></img>
+                                        </a>
+                                      </div>
+                                      <div className='author-con'>
+                                        <div className='author-item-title'>
+                                          <a className='author-a' href=''>
+                                            <span className='author-span'>Anna Hilltons</span>
+                                          </a>
+                                        </div>
+                                        <div className='author-item-count'>
+                                          4
+                                          <span className='author-book'> Published Book</span>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <div className='author-time'>
+                                    <div className='author-time-content'>
+                                      <div className='author-item-image'>
+                                        <a className='author-href'href='#'>
+                                          <img decoding='async' className='author-img' src='https://wpbingosite.com/wordpress/bootin/wp-content/uploads/2019/05/Image-4-4.jpg' alt='Coco Simon'></img>
+                                        </a>
+                                      </div>
+                                      <div className='author-con'>
+                                        <div className='author-item-title'>
+                                          <a className='author-a' href=''>
+                                            <span className='author-span'>Coco Simon</span>
+                                          </a>
+                                        </div>
+                                        <div className='author-item-count'>
+                                          2
+                                          <span className='author-book'> Published Book</span>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <div className='author-time'>
+                                    <div className='author-time-content'>
+                                      <div className='author-item-image'>
+                                        <a className='author-href'href='#'>
+                                          <img decoding='async' className='author-img' src='https://wpbingosite.com/wordpress/bootin/wp-content/uploads/2019/05/Image-3-3.jpg' alt='Nicolas Sumpir'></img>
+                                        </a>
+                                      </div>
+                                      <div className='author-con'>
+                                        <div className='author-item-title'>
+                                          <a className='author-a' href=''>
+                                            <span className='author-span'>Nicolas Sumpir</span>
+                                          </a>
+                                        </div>
+                                        <div className='author-item-count'>
+                                          3
+                                          <span className='author-book'> Published Book</span>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <div className='author-time'>
+                                    <div className='author-time-content'>
+                                      <div className='author-item-image'>
+                                        <a className='author-href'href='#'>
+                                          <img decoding='async' className='author-img' src='https://wpbingosite.com/wordpress/bootin/wp-content/uploads/2019/07/Image-7-7.jpg' alt='Oga Sylas'></img>
+                                        </a>
+                                      </div>
+                                      <div className='author-con'>
+                                        <div className='author-item-title'>
+                                          <a className='author-a' href=''>
+                                            <span className='author-span'>Oga Sylas</span>
+                                          </a>
+                                        </div>
+                                        <div className='author-item-count'>
+                                          0
+                                          <span className='author-book'> Published Book</span>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <div className='author-time'>
+                                    <div className='author-time-content'>
+                                      <div className='author-item-image'>
+                                        <a className='author-href'href='#'>
+                                          <img decoding='async' className='author-img' src='https://wpbingosite.com/wordpress/bootin/wp-content/uploads/2019/07/Image-8-8.jpg' alt='Peter Paker'></img>
+                                        </a>
+                                      </div>
+                                      <div className='author-con'>
+                                        <div className='author-item-title'>
+                                          <a className='author-a' href=''>
+                                            <span className='author-span'>Peter Paker</span>
+                                          </a>
+                                        </div>
+                                        <div className='author-item-count'>
+                                          0
+                                          <span className='author-book'> Published Book</span>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <div className='author-time'>
+                                    <div className='author-time-content'>
+                                      <div className='author-item-image'>
+                                        <a className='author-href'href='#'>
+                                          <img decoding='async' className='author-img' src='https://wpbingosite.com/wordpress/bootin/wp-content/uploads/2019/05/Image-5-5.jpg' alt='Sabela Hupter'></img>
+                                        </a>
+                                      </div>
+                                      <div className='author-con'>
+                                        <div className='author-item-title'>
+                                          <a className='author-a' href=''>
+                                            <span className='author-span'>Sabela Hupter</span>
+                                          </a>
+                                        </div>
+                                        <div className='author-item-count'>
+                                          7
+                                          <span className='author-book'> Published Book</span>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <div className='author-time'>
+                                    <div className='author-time-content'>
+                                      <div className='author-item-image'>
+                                        <a className='author-href'href='#'>
+                                          <img decoding='async' className='author-img' src='https://wpbingosite.com/wordpress/bootin/wp-content/uploads/2019/05/Image-6-6.jpg' alt='Susan Merrlin'></img>
+                                        </a>
+                                      </div>
+                                      <div className='author-con'>
+                                        <div className='author-item-title'>
+                                          <a className='author-a' href=''>
+                                            <span className='author-span'>Susan Merrlin</span>
+                                          </a>
+                                        </div>
+                                        <div className='author-item-count'>
+                                          4
+                                          <span className='author-book'> Published Book</span>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
